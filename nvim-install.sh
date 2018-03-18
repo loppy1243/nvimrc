90cabb8d346504ad06d1b1574d56cab2ace7d32a
Nicholas Todoroff <loppy1243@gmail.com>
2018-03-18 11:12:22 -0400

    Make nvim-install.sh work on mobile

--- nvim-install.sh
+++ nvim-install.sh
@@ -10,11 +10,11 @@ if [ -e ~/.config/nvim ]; then
     read response
     if [ $response = 'y' -o $response = 'Y' ]; then
       rm -d ~/.config/nvim
-      ln -Ts $(readlink -en $(dirname "$0")) ~/.config/nvim
+      ln -Ts $(readlink -fn $(dirname "$0")) ~/.config/nvim
     fi
   elif ! [ -L ~/.config/nvim ]; then
     echo "$HOME/.config/nvim is not a symlink and not empty. Figure it out yourself."
   fi
 else
-  ln -Ts $(readlink -en $(dirname "$0")) ~/.config/nvim
+  ln -Ts $(readlink -fn $(dirname "$0")) ~/.config/nvim
 fi
