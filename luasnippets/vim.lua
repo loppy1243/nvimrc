997233281e734a8eedbee820bcab64796c401f6b
Nicholas Todoroff <nd.todoroff@gmail.com>
2024-01-16 13:09:59 -0700

    Improve VIMscript 'func' snippet trigger condition

--- luasnippets/vim.lua
+++ luasnippets/vim.lua
@@ -3,7 +3,14 @@ local function reg_match(n)
 end
 
 return {
-  s({trig="func", regTrig=true, wordTrig=false, snippetType="autosnippet"},
+  s({trig="%sfunc", regTrig=true, wordTrig=false, snippetType="autosnippet"},
+    fmt([[
+          {}function! {}({})
+            {}{}
+          {}endfunction
+        ]],
+        { f(reg_match(1)), i(1), i(2), f(reg_match(1)), i(0), f(reg_match(1)) })),
+  s({trig="^func", regTrig=true, wordTrig=false, snippetType="autosnippet"},
     fmt([[
           {}function! {}({})
             {}{}
